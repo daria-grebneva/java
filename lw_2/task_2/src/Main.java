a2e031a55e3c0b8c0d0631b74c8a6fd73ccd28da
daria-grebneva <greda2598@gmail.com>
2018-10-26 01:13:57 +0300

    Добавила часть 2

--- lw_2/task_2/src/Main.java
+++ lw_2/task_2/src/Main.java
@@ -1,9 +1,10 @@
+//import com.sun.org.apache.xpath.internal.operations.String;
+
 import java.util.List;
 import java.util.Arrays;
 
 public class Main {
-    char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
 
     public static void main(String[] args) {
         if (!isCorrectArgsNumber(args)) {
@@ -17,20 +18,68 @@ public class Main {
         processOperation(args[0], args[1], args[2]);
     }
 
+    private static char[] getAlphabet()
+    {
+        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        return alphabet;
+    }
+
     private static String processOperation(String operation, String key, String str) {
-        if (operation == "-e") {
+        if (operation.equals("-e")) {
             //шифрование
+            encryption(key, str);
         } else {
             //дешифрование
         }
         return "";
     }
 
-    private static String encryption(String key, String str) {
-        for (int i = 0; i < str.length(); i++) {
-            str.charAt(i); //-находим в массиве, потом находим число сдвинутое в массиве на одну позицию
+    private static String encryption(String key1, String str) {
+        int key = Integer.parseInt(key1);
+//        String newString = new String();
+//        for(int i = 0; i < str.length(); i++) {
+//            boolean isLowerCase = true;
+//            char letter = str.charAt(i);
+//
+//            if (!(letter >= 'a' && letter <= 'z')) {
+//                isLowerCase = false;
+//                letter = Character.toLowerCase(letter);
+//            }
+//
+//            if(Character.isLetter((char) ((int) letter + key)))
+//                newString += ((char) ((int) letter + key));
+//            else{
+//                int keyCheck = key;
+//                keyCheck -= (int)'z' -  (int) letter;
+//                keyCheck = keyCheck % 26;
+//                if(keyCheck != 0) {
+//                    if (isLowerCase)
+//                        newString += ((char) ((int) 'a' + keyCheck - 1));
+//                    else
+//                        newString += (Character.toUpperCase ((char) ((int) 'a' + keyCheck - 1)));
+//                }
+//                else {
+//                    if (isLowerCase)
+//                        newString += 'z';
+//                    else
+//                        newString += 'Z';
+//                }
+//            }
+//        }
+        int a;
+        StringBuffer res = new StringBuffer();
+
+        try {
+            for(int i = 0; i < str.length(); i++) {
+                res.append(Character.toString((char) (str.charAt(i) + key)));
+            }
+        } catch (Exception e) {
+            return new String();
         }
-        return "";
+
+        return res.toString();
+//        return newString;
     }
 
     private static boolean findElemInArray(char[] alphabet, char elem) {
@@ -39,8 +88,7 @@ public class Main {
 
 
     private static boolean isCorrectOperationMode(String operation) {
-        System.out.println(operation);
-        return ((operation == "-e") || (operation == "-d"));
+        return (operation.equals("-e") || operation.equals("-d"));
     }
 
     private static boolean isCorrectArgsNumber(String[] args) {
