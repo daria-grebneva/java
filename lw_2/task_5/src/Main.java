0cd8e24771868dd325b90e9d5c2c6025da6687f9
daria-grebneva <greda2598@gmail.com>
2018-10-29 10:17:13 +0300

    Добавила пятую л/р

--- lw_2/task_5/src/Main.java
+++ lw_2/task_5/src/Main.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
         if (!isCorrectArgsNumber(args)) {
             System.out.println("Incorrect number of arguments");
@@ -12,27 +13,25 @@ public class Main {
             System.exit(0);
         }
 
-        Map<String, Integer> incidence = readFromFile(args[0]);
-        Map<String, Integer> orderedMap = sortMap(incidence);
-        printWords( orderedMap, Integer.valueOf(args[1]));
+        ArrayList<String> wordsList = new ArrayList<>();
+        Map<String, Integer> incidence = readFromFile(args[0], wordsList);
+        printWords(incidence, Integer.valueOf(args[1]), wordsList);
     }
 
-    private static void printWords( Map<String, Integer> orderedMap, int quantity)
-    {
+    private static void printWords(Map<String, Integer> orderedMap, int quantity, ArrayList<String> wordsList) {
         int i = 0;
-        for(Map.Entry<String, Integer> e : orderedMap.entrySet()) {
-            System.out.println(e.getKey() + " " + e.getValue());
+        for (int j = (wordsList.size() - 1); j >= 0; j--) {
+            String temp = wordsList.get(j);
+            System.out.println(temp + " " + orderedMap.get(temp));
             ++i;
-            if (i == quantity)
-            {
+            if (i == quantity) {
                 break;
             }
         }
     }
 
-    private static Map<String, Integer> readFromFile(String fileName) {
+    private static Map<String, Integer> readFromFile(String fileName, ArrayList<String> wordsList) {
         Map<String, Integer> incidence = new HashMap<String, Integer>();
-
         Scanner scanner = null;
         try {
             scanner = new Scanner(new File(fileName));
@@ -45,23 +44,30 @@ public class Main {
             String line = scanner.nextLine();
             String[] string = line.split(" ");
             for (String v : string) {
+                boolean repeatWord = false;
                 if (incidence.containsKey(v)) {
                     incidence.put(v, incidence.get(v) + 1);
+                    repeatWord = true;
                 } else {
                     incidence.put(v, j);
                 }
+                addWordToList(v, wordsList, repeatWord);
             }
         }
+
         return incidence;
     }
 
-    private static  Map<String, Integer> sortMap( Map<String, Integer> map) {
-        Map<String, Integer> orderedMap = map.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .collect(LinkedHashMap::new,
-                        (m, c) -> m.put(c.getKey(), c.getValue()),
-                        LinkedHashMap::putAll);
-        return orderedMap;
+    private static void addWordToList(String word, ArrayList<String> wordsList, boolean repeatWord) {
+        if (repeatWord) {
+            int index = wordsList.indexOf(word);
+            if (index != (wordsList.size() - 1)) {
+                wordsList.remove(word);
+                wordsList.add(index + 1, word);
+            }
+        } else {
+            wordsList.add(0, word);
+        }
     }
 
     private static boolean isCorrectArgsNumber(String[] args) {
