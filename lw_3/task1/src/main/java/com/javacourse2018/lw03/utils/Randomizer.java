2879784cbdc75b3c98fdc5297a426117399208ea
daria-grebneva <greda2598@gmail.com>
2018-12-01 10:22:20 +0300

    Убрала неиспользуемую утилиту

--- lw_3/task1/src/main/java/com/javacourse2018/lw03/utils/Randomizer.java
+++ lw_3/task1/src/main/java/com/javacourse2018/lw03/utils/Randomizer.java
@@ -7,10 +7,5 @@ import java.util.Random;
 public class Randomizer {
     public static int getRandomInt(int lowerBound, int upperBound) {
         return (int) (Math.random() * (upperBound - lowerBound)) + lowerBound;
-    }
-
-    public static int getRandomFromArray(int[] array) {
-        int rnd = new Random().nextInt(array.length);
-        return array[rnd];
-    }
+    }       
 }
